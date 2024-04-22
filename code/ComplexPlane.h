ff4c91d5401b148abbe06972f66a0bf287fae4ac
Your Name <you@example.com>
2024-04-22 15:30:50 -0700

    added function prototypes to h file via copypaste?

--- code/ComplexPlane.h
+++ code/ComplexPlane.h
@@ -15,13 +15,28 @@ class ComplexPlane : public Drawable
 {
     public:
 
+    ComplexPlane::ComplexPlane(int pixelWidth, int pixelHeight);
+    void ComplexPlane::draw(RenderTarget& target, RenderStates states) const;
+    void complexPlane::updateRender();
+    void ComplexPlane::zoomIn();
+    void ComplexPlane::zoomOut();
+    void ComplexPlane::setCenter(Vector2i mousePixel);
+    void ComplexPlane::setMouseLocation(Vector2i mousePixel);
+    void ComplexPlane::loadText(Text& text);
+
+
+
     private:
-        VertexArray m_Array;
-        State m_state;
-        Vector 2f m_mouseLocation;
-        Vector 2i m_pixel_size;
-        Vector 2f m_plane_center;
-        Vector 2f m_plane_size;
-        int m_zoomCount;
-        float m_aspectRatio;
+    VertexArray m_Array;
+    State m_state;
+    Vector 2f m_mouseLocation;
+    Vector 2i m_pixel_size;
+    Vector 2f m_plane_center;
+    Vector 2f m_plane_size;
+    int m_zoomCount;
+    float m_aspectRatio;
+
+    size_t ComplexPlane::countIterations(Vector2f coord);
+    void ComplexPlane::iterationsToRGB(size_t count, Uint8& r, Uint8& g, Uint8& b);
+    Vector2f ComplexPlane::mapPixelToCoords(Vector2i mousePixel);
 }
---
+++ code/main.cpp
@@ -0,0 +1,25 @@
+#include "ComplexPlane.h"
+using namespace std;
+
+
+ComplexPlane::ComplexPlane(int pixelWidth, int pixelHeight);
+
+void ComplexPlane::draw(RenderTarget& target, RenderStates states) const;
+
+void complexPlane::updateRender();
+
+void ComplexPlane::zoomIn();
+
+void ComplexPlane::zoomOut();
+
+void ComplexPlane::setCenter(Vector2i mousePixel);
+
+void ComplexPlane::setMouseLocation(Vector2i mousePixel);
+
+void ComplexPlane::loadText(Text& text);
+
+size_t ComplexPlane::countIterations(Vector2f coord);
+
+void ComplexPlane::iterationsToRGB(size_t count, Uint8& r, Uint8& g, Uint8& b);
+
+Vector2f ComplexPlane::mapPixelToCoords(Vector2i mousePixel);

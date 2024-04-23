87d48f600f3d96f0de3afdeb9f8d6fa0261b6fdc
Nigel Santiago <166557965+nigelsantiago@users.noreply.github.com>
2024-04-23 00:49:58 -0700

    Updated main.cpp

--- code/main.cpp
+++ code/main.cpp
@@ -7,7 +7,56 @@
 using namespace std;
 using namespace sf;
 
-unsigned int desktop_w = VideoMode::getDesktopMode().width / 2;    // 2
-unsigned int desktop_h = VideoMode::getDesktopMode().height / 2;    // divided by 2 temp edit
-VideoMode vm(desktop_w, desktop_h);
-RenderWindow window(vm, "Mandelbrot Set", Style::Default);
+int main()
+{
+    unsigned int desktop_w = VideoMode::getDesktopMode().width / 2;    //  /2
+    unsigned int desktop_h = VideoMode::getDesktopMode().height / 2;    // divided by 2 temp edit only or use sep var?
+    VideoMode vm(desktop_w, desktop_h);
+    
+    RenderWindow window(vm, "Mandelbrot Set", Style::Default);
+    ComplexPlane complexPlane(desktop_w, desktop_h);
+    
+    Text text;
+    Font font;
+    if (!font.loadFromFile("fonts/Pixelate.ttf"))
+    {
+        cout << "Error loading font file." << endl;
+    }
+    text.setFont(font);
+    text.setCharacterSize(45);
+    text.setPosition(desktop_w/4.0f, desktop_h/4.0f);
+    text.setFillColor(Color::White);
+    text.setStyle(Text::Bold);
+
+    while (window.isOpen())
+	{
+        Event event;
+        while (window.pollEvent(event))
+		{
+            if (event.type == Event::Closed)
+            {
+                window.close();
+            }
+            else if (event.type == Event::MouseButtonPressed) // fill in
+            {
+            }
+            else if (event.type == Event::MouseeMoved) // fill in
+            {
+            }
+            else if (Keyboard::isKeyPressed(Keyboard::Escape))
+            {
+                window.close();
+            }
+        }
+        
+        updateRender();
+        loadText(text);
+        
+        window.clear();
+        window.draw(complexPlane);
+        window.draw(text);
+        window.display();
+    }
+
+    return 0;
+}

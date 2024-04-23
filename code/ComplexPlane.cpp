600e8985bef16447428d992eac3600e192870636
raskaurr <166560815+raskaurr@users.noreply.github.com>
2024-04-22 18:40:39 -0700

    Update ComplexPlane.cpp

--- code/ComplexPlane.cpp
+++ code/ComplexPlane.cpp
@@ -8,17 +8,42 @@ using namespace sf;
 
 ComplexPlane::ComplexPlane(int pixelWidth, int pixelHeight)
 {
+    m_pixel_size.x = pixelWidth;
+    m_pixel_size.y = pixelHeight;
 
+    m_aspectRatio = pixelHeight / pixelWidth; 
+
+    m_plane_center = (0,0);
+
+    m_plane_size= (BASE_WIDTH, BASE_HEIGHT * m_aspectRatio);
+
+    m_zoomCount = 0;
+
+    m_state = State::CALCULATING;
+    //Still need to intialize vertex array
 }
 
 void ComplexPlane::draw(RenderTarget& target, RenderStates states) const
 {
-
+    target.draw(m_vArray);
 }
 
 void complexPlane::updateRender()
 {
-
+    if (m_state = CAlCULATING)
+    {
+	    for (int i = 0; i < pixelHeight; ++i)
+	    {
+	        for (int j = 0; j < pixelWidth; ++i)
+		    {
+		        vArray[j + i * pixelWidth].position = {(float)j,(float)i};
+			    m_plane_size = ComplexPlane::mapPixelToCoords(mousePixel);
+			    int count = ComplexPlane::countIterations(m_mouseLocation);
+			    //store RGB values for current pixel
+		    }
+	    }
+    }
+    m_state = DISPLAYING;
 }
 
 void ComplexPlane::zoomIn()

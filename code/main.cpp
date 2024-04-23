cb341af1d1cd11be89ef19d3d703cf698dd89444
Nigel Santiago <166557965+nigelsantiago@users.noreply.github.com>
2024-04-23 00:29:21 -0700

    Updated main.cpp

--- code/main.cpp
+++ code/main.cpp
@@ -6,3 +6,8 @@
 #include "ComplexPlane.h"
 using namespace std;
 using namespace sf;
+
+unsigned int desktop_w = VideoMode::getDesktopMode().width / 2;    // 2
+unsigned int desktop_h = VideoMode::getDesktopMode().height / 2;    // divided by 2 temp edit
+VideoMode vm(desktop_w, desktop_h);
+RenderWindow window(vm, "Mandelbrot Set", Style::Default);
